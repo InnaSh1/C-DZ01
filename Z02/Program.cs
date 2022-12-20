5286a46037ba20abdee54dbc58517b4c57b1df41
inna <inna.makul@gmail.com>
2022-12-21 01:00:49 +0300

    создали решение 2

---
+++ Z02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+
+Console.WriteLine ("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine()); 
+
+Console.WriteLine ("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine()); 
+
+Console.WriteLine ("Введите число C: ");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.WriteLine(max);
